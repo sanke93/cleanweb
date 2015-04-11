9064d6151a7c5ddabe50a576362db6749ef65e45
Sanket Chauhan <sanke93@gmail.com>
2015-04-11 16:05:52 -0400

    added geolocation marker

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -52,91 +52,61 @@ angular.module('ionicParseApp.controllers', [])
     console.log("Hi", $scope.user.attributes.email);
     scope_trip = $scope;
     initialize = function () {
-      var site = new google.maps.LatLng(55.9879314,-4.3042387);
-      var hospital = new google.maps.LatLng(55.8934378,-4.2201905);
+      //var startlocation = new google.maps.LatLng(55.9879314,-4.3042387);
+      var startLocation = new google.maps.LatLng(42.3503446, -71.0571948)
+      //var end = new google.maps.LatLng(55.8934378,-4.2201905);
 
       var mapOptions = {
         streetViewControl:true,
-        center: site,
+        center: startLocation,
         zoom: 18,
-        mapTypeId: google.maps.MapTypeId.TERRAIN
+        //mapTypeId: google.maps.MapTypeId.TERRAIN
       };
       var map = new google.maps.Map(document.getElementById("map"),
           mapOptions);
 
       //Marker + infowindow + angularjs compiled ng-click
-      var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-      var compiled = $compile(contentString)($scope);
-
-      var infowindow = new google.maps.InfoWindow({
-        content: compiled[0]
-      });
-
-      var marker = new google.maps.Marker({
-        position: site,
-        map: map,
-        title: 'Strathblane (Job Location)'
-      });
-
-      var hospitalRoute = new google.maps.Marker({
-        position: hospital,
-        map: map,
-        title: 'Hospital (Stobhill)'
-      });
-
-      var infowindow = new google.maps.InfoWindow({
-           content:"Project Location"
-      });
-
-      infowindow.open(map,marker);
-
-      var hospitalwindow = new google.maps.InfoWindow({
-           content:"Nearest Hospital"
-      });
-
-      hospitalwindow.open(map,hospitalRoute);
-
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map,marker);
-      });
+      
 
       $scope.map = map;
+      var GeoMarker = new GeolocationMarker(map);
 
-      var directionsService = new google.maps.DirectionsService();
-      var directionsDisplay = new google.maps.DirectionsRenderer();
+      // var directionsService = new google.maps.DirectionsService();
+      // var directionsDisplay = new google.maps.DirectionsRenderer();
 
-      var request = {
-          origin : site,
-          destination : hospital,
-          travelMode : google.maps.TravelMode.DRIVING
-      };
-      directionsService.route(request, function(response, status) {
-          if (status == google.maps.DirectionsStatus.OK) {
-              directionsDisplay.setDirections(response);
-          }
-      });
+      // var request = {
+      //     origin : site,
+      //     destination : hospital,
+      //     travelMode : google.maps.TravelMode.DRIVING
+      // };
+      // directionsService.route(request, function(response, status) {
+      //     if (status == google.maps.DirectionsStatus.OK) {
+      //         directionsDisplay.setDirections(response);
+      //     }
+      // });
 
-      directionsDisplay.setMap(map);
+      // directionsDisplay.setMap(map);
     }
 
 
     google.maps.event.addDomListener(window, 'load', initialize());
 
     $scope.centerOnMe = function() {
-    if(!$scope.map) {
-      return;
-    }
+        if(!$scope.map) {
+          return;
+        }
 
-    $scope.loading = $ionicLoading.show({
-      content: 'Getting current location...',
-      showBackdrop: false
-    });
-    navigator.geolocation.getCurrentPosition(function(pos) {
-      $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-      $scope.loading.hide();
-    }, function(error) {
-      alert('Unable to get location: ' + error.message);
-    });
+        $scope.loading = $ionicLoading.show({
+          content: 'Getting current location...',
+          template: 'hi',
+          showBackdrop: true
+        });
+        navigator.geolocation.getCurrentPosition(function(pos) {
+          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+          $scope.loading.hide();
+        }, function(error) {
+          alert('Unable to get location: ' + error.message);
+        });
     };
 
     $scope.clickTest = function() {
